a109fc1424930f4f523286ab2e29c6a5ed7ee9b3
H. Joe Lee <hyoklee@hdfgroup.org>
2026-04-30 18:00:34 -0500

    chore: clean up test scripts

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hyoklee/content-assimilation-engine
-# Build directory: /home/hyoklee/content-assimilation-engine/omni
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
